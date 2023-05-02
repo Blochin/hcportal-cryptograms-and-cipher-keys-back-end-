da6db35d69ec44334a71a2008c69c3c3993596ca
Ati <attila.mucska@gmail.com>
2023-05-02 22:14:36 +0200

    fix: seeder

--- database/seeders/CipherAndKeyTypeSeeder.php
+++ database/seeders/CipherAndKeyTypeSeeder.php
@@ -17,26 +17,13 @@ class CipherAndKeyTypeSeeder extends Seeder
     public function run()
     {
 
-        DB::statement('SET FOREIGN_KEY_CHECKS=0;');
-        DB::table('cipher_types')->truncate();
         DB::table('key_types')->truncate();
         DB::statement('SET FOREIGN_KEY_CHECKS=1;');
-
-        $cipherTypes = [
-            ['name' => 'Undefined'],
-            ['name' => 'Nomenclator'],
-            ['name' => 'Code'],
-            ['name' => 'Simple substitution']
-        ];
 
         $keyTypes = [
             ['name' => 'e'],
             ['name' => 'ed'],
         ];
-
-        foreach ($cipherTypes as $type) {
-            CipherType::create($type);
-        }
 
         foreach ($keyTypes as $type) {
             KeyType::create($type);
